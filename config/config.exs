029c6c0e3e4f2f41c8530ebfb84d8213f8d49e55
GeoffreyPS <geoffreyps@gmail.com>
2022-06-02 19:15:53 -0600

    Remove config.exs

--- config/config.exs
+++
@@ -1,5 +0,0 @@
-use Mix.Config
-
-# You shouldn't set configuration for this library in the Mix config here, since
-# it's not carried over when the library is used as a dependency. This is only useful
-# for setting test configuration here.
